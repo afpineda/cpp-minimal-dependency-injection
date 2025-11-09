4578edd0f7fe227a85f7aba87bebdcb3fc83b598
afpineda <74291754+afpineda@users.noreply.github.com>
2025-11-09 20:33:14 +0100

    Rewrite provider sets

--- Examples/ProviderSetExample.cpp
+++ Examples/ProviderSetExample.cpp
@@ -9,88 +9,52 @@
 #include <iostream>
 
 // Import the framework
-#include "../InternalServices.hpp"
-using namespace InternalServices;
+#include "../dip.hpp"
 
-// Declare an abstract class as a self-managed service.
-class SERVICE(MyServiceInterface)
+// Declare a service.
+class MyService
 {
 public:
-    virtual void doSomething() = 0;
+    virtual void foo() = 0;
+    virtual ~MyService() {};
 };
 
-// A provider for MyServiceInterface.
-// Instance addresses are logged to evidence
-// the lifetime of service provider instances.
-class MyServiceProvider1 : public MyServiceInterface
+// Declare a service provider.
+class MyServiceProvider1 : public MyService
 {
 public:
-    virtual void doSomething() override
+    virtual void foo() override
     {
-        std::cout << this << ".MyServiceProvider1::doSomething()" << std::endl;
+        std::cout << this << ".MyServiceProvider1::foo()" << std::endl;
     };
 };
 
-// Another provider for MyServiceInterface.
-// Instance addresses are logged to evidence
-// the lifetime of service provider instances.
-class MyServiceProvider2 : public MyServiceInterface
+// Declare another service provider.
+class MyServiceProvider2 : public MyService
 {
 public:
-    virtual void doSomething() override
+    virtual void foo() override
     {
-        std::cout << this << ".MyServiceProvider2::doSomething()" << std::endl;
+        std::cout << this << ".MyServiceProvider2::foo()" << std::endl;
     };
 };
 
-// A consumer of the MyServiceInterface service
-// for demonstration purposes.
-// A consumer class is not mandatory.
-//
-// This class will invoke all available service
-// providers, which are MyServiceProvider1 and
-// MyServiceProvider2 in this example.
-class MyConsumer
+// Consume a set of service providers
+void test()
 {
-public:
-    MyServiceInterface::ProviderSet services;
-    MyConsumer()
+    dip::instance_set<MyService> instance_set;
+    for (std::size_t i = 0; i < instance_set.size(); i++)
     {
-        services = MyServiceInterface::getAllInstances();
+        instance_set[i]->foo();
     }
-
-    void runService()
-    {
-        for (auto service : services)
-            service->doSomething();
-    }
-};
+}
 
 int main()
 {
-    std::cout << "-- main begin" << std::endl;
+    // Inject
+    dip::add_singleton<MyService, MyServiceProvider1>();
+    dip::add_singleton<MyService, MyServiceProvider2>();
 
-    // We inject both service providers:
-    // MyServiceProvider1 first, then MyServiceProvider2.
-    // Note that instances from MyServiceProvider1 will
-    // have a different lifetime than instances
-    // from MyServiceProvider2.
-    MyServiceInterface::inject<
-        MyServiceProvider1,
-        Lifetime::Singleton,
-        ServiceConsumerMode::getAllInstances>();
-    MyServiceInterface::inject<
-        MyServiceProvider2,
-        Lifetime::Transient,
-        ServiceConsumerMode::getAllInstances>();
-
-    // There are two consumers running the service
-    // from all service providers.
-    MyConsumer consumer1, consumer2;
-    std::cout << ".. consumer1 .." << std::endl;
-    consumer1.runService();
-    std::cout << ".. consumer2 .." << std::endl;
-    consumer2.runService();
-
-    std::cout << "-- main end" << std::endl;
+    // Consume
+    test();
 }
--- dip.hpp
+++ dip.hpp
@@ -17,6 +17,7 @@
 #include <type_traits>
 #include <cassert>
 #include <functional>
+#include <vector>
 
 // #include <iostream> // For testing
 
@@ -206,13 +207,15 @@ namespace dip
     private:
         service_type *_instance = nullptr;
         inline static Injector<Service> _injector;
-    };
+    }; // struct instance
 
     /**
      * @brief Inject a service provider using a custom injector
      *
+     * @note To be consumed using std::instance<Service>
+     *
      * @tparam Service Injectable service
-     *  @param injector Service injector
+     * @param injector Service injector
      */
     template <class Service>
     inline void inject(const Injector<Service> &injector)
@@ -222,6 +225,8 @@ namespace dip
 
     /**
      * @brief Inject a transient instance to a Service
+     *
+     * @note To be consumed using std::instance<Service>
      *
      * @tparam Service Injectable service
      * @tparam Provider Service provider
@@ -236,6 +241,8 @@ namespace dip
 
     /**
      * @brief Inject a singleton instance to a Service
+     *
+     * @note To be consumed using std::instance<Service>
      *
      * @tparam Service Injectable service
      * @tparam Provider Service provider
@@ -261,4 +268,236 @@ namespace dip
     {
         instance<Service>::template inject_thread_singleton<Provider>(std::forward<_Args>(args)...);
     }
-}
+
+    /**
+     * @brief Set of injected instances of a service
+     *
+     * @tparam Service Service to be injected
+     */
+    template <class Service>
+    struct instance_set
+    {
+        static_assert(std::is_abstract<Service>::value, "Only abstract classes are injectable");
+        static_assert(std::has_virtual_destructor<Service>::value, "An injectable service must declare a virtual destructor");
+
+        typedef Service service_type;
+
+        /**
+         * @brief Retrieve a set of instances providing the service
+         *
+         */
+        instance_set()
+        {
+            assert(!_injectors.empty() && "No dependency injections");
+            for (auto injector : _injectors)
+            {
+                assert(injector.retrieve && "Missing dependency injection");
+                auto instance = injector.retrieve();
+                assert(instance && "An injector retrieved a null provider");
+                _instances.push_back(instance);
+            }
+        }
+
+        /**
+         * @brief Remove the set of instances providing the service
+         *
+         */
+        ~instance_set() noexcept
+        {
+            for (std::size_t i = 0; i < _injectors.size(); i++)
+                if (_injectors[i].forget)
+                    _injectors[i].forget(_instances.at(i));
+        }
+
+        instance_set(const instance_set &&) = delete;
+        instance_set(instance_set &&) = delete;
+        instance_set &operator=(const instance_set &) = delete;
+        instance_set &operator=(instance_set &&) = delete;
+
+        /**
+         * @brief Get the count of instances injected into the service
+         *
+         * @return constexpr std::size_t Count of instances
+         */
+        constexpr std::size_t size() const noexcept
+        {
+            return _instances.size();
+        }
+
+        service_type *operator[](std::size_t index)
+        {
+            return _instances[index];
+        }
+
+        const service_type *operator[](std::size_t index) const
+        {
+            return _instances[index];
+        }
+
+        service_type *at(std::size_t index)
+        {
+            return _instances.at(index);
+        }
+
+        const service_type *at(std::size_t index) const
+        {
+            return _instances.at(index);
+        }
+
+        /**
+         * @brief Inject a service provider using a custom injector
+         *
+         * @param injector Service injector
+         */
+        static void add(const Injector<Service> &injector) noexcept
+        {
+            assert(injector.retrieve && "Invalid injector");
+            _injectors.push_back(injector);
+        }
+
+        /**
+         * @brief Inject a service provider with singleton life cycle
+         *
+         * @tparam Provider Service provider
+         * @tparam _Args Constructor parameter types
+         * @param __args Constructor parameters
+         */
+        template <class Provider, typename... _Args>
+        static void add_singleton(_Args &&...__args)
+        {
+            static_assert(std::is_base_of<Service, Provider>::value, "Provider does not implement Service");
+            Injector<Service> injector{
+                .retrieve = [... args = std::forward<_Args>(__args)]() -> Service *
+                {
+                    static Provider p(args...);
+                    return &p;
+                }};
+            add(injector);
+        }
+
+        /**
+         * @brief Inject a service provider with per-thread singleton life cycle
+         *
+         * @tparam Provider Service provider
+         * @tparam _Args Constructor parameter types
+         * @param __args Constructor parameters
+         */
+        template <class Provider, typename... _Args>
+        static void add_thread_singleton(_Args &&...__args)
+        {
+            static_assert(std::is_base_of<Service, Provider>::value, "Provider does not implement Service");
+            Injector<Service> injector{
+                .retrieve = [... args = std::forward<_Args>(__args)]() -> Service *
+                {
+                    static thread_local Provider p(args...);
+                    return &p;
+                }};
+            add(injector);
+        }
+
+        /**
+         * @brief Inject a service provider with transient life cycle
+         *
+         * @tparam Provider Service provider
+         * @tparam _Args Constructor parameter types
+         * @param __args Constructor parameters
+         */
+        template <class Provider, typename... _Args>
+        static void add_transient(_Args &&...__args)
+        {
+            static_assert(std::is_base_of<Service, Provider>::value, "Provider does not implement Service");
+            Injector<Service> injector
+            {
+                .retrieve = [... args = std::forward<_Args>(__args)]() -> Service *
+                {
+                    static thread_local Provider p(args...);
+                    return &p;
+                },
+                .forget = [](Service *provider) -> void
+                {
+                    delete provider;
+                }
+            };
+            add(injector);
+        }
+
+        /**
+         * @brief Clear all the injected dependencies for testing purposes
+         *
+         * @warning Do not call in production code.
+         *          Will cause memory leaks if transient instances are not destroyed before calling.
+         *
+         */
+        static void clear_injections() noexcept
+        {
+            _injectors.clear();
+        }
+
+    private:
+        std::vector<service_type *> _instances;
+        inline static std::vector<Injector<Service>> _injectors;
+    }; // struct instances
+
+    /**
+     * @brief Inject a service provider using a custom injector
+     *
+     * @note To be consumed using std::instance_set<Service>
+     *
+     * @tparam Service Injectable service
+     *  @param injector Service injector
+     */
+    template <class Service>
+    inline void add(const Injector<Service> &injector)
+    {
+        instance_set<Service>::add(injector);
+    }
+
+    /**
+     * @brief Inject a transient instance to a Service
+     *
+     * @note To be consumed using std::instance_set<Service>
+     *
+     * @tparam Service Injectable service
+     * @tparam Provider Service provider
+     * @tparam _Args Constructor argument types
+     * @param args Constructor arguments
+     */
+    template <class Service, class Provider, typename... _Args>
+    inline void add_transient(_Args &&...args)
+    {
+        instance_set<Service>::template add_transient<Provider>(std::forward<_Args>(args)...);
+    }
+
+    /**
+     * @brief Inject a singleton instance to a Service
+     *
+     * @note To be consumed using std::instance_set<Service>
+     *
+     * @tparam Service Injectable service
+     * @tparam Provider Service provider
+     * @tparam _Args Constructor argument types
+     * @param args Constructor arguments
+     */
+    template <class Service, class Provider, typename... _Args>
+    inline void add_singleton(_Args &&...args)
+    {
+        instance_set<Service>::template add_singleton<Provider>(std::forward<_Args>(args)...);
+    }
+
+    /**
+     * @brief Inject a per-thread singleton instance to a Service
+     *
+     * @note To be consumed using std::instance_set<Service>
+     *
+     * @tparam Service Injectable service
+     * @tparam Provider Service provider
+     * @tparam _Args Constructor argument types
+     * @param args Constructor arguments
+     */
+    template <class Service, class Provider, typename... _Args>
+    inline void add_thread_singleton(_Args &&...args)
+    {
+        instance_set<Service>::template add_thread_singleton<Provider>(std::forward<_Args>(args)...);
+    }
+
+}; // namespace dip
